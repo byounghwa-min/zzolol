00fe9a5b22da99e7dc9b2523d5a2559772f767af
byounghwa-min <dpatod1418@naver.com>
2025-03-05 15:32:42 +0900

    자동 커밋

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, query, onSnapshot, serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyAVwuvwl4OJl_JWzYHno9Y_kDLyWvfX0Is",
+    authDomain: "zzolol.firebaseapp.com",
+    projectId: "zzolol",
+    storageBucket: "zzolol.firebasestorage.app",
+    messagingSenderId: "63120194972",
+    appId: "1:63120194972:web:b1f4927797abccb7531c59"
+  };
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
